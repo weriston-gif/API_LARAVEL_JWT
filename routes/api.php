25047e06b1e32fd39aa5c017f37335cd61a78792
Weriston Alves <weristonalves1@gmail.com>
2023-07-11 17:48:21 -0300

    versionamento

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'api_name' => 'Laravel API + JWT',
-        'api_version' => '1.0.0'
+        'api_name' => 'Laravel API + JWT.',
+        'api_version' => '1.0.1'
     ]);
 });
 
